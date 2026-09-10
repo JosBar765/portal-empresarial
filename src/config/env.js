5520b190c15c88745157a5d9fdfdc2d0e81e8a24
JosBar765 <2019pcdell@gmail.com>
2026-09-10 10:18:46 -0600

    cambiando tiempo de expiracion de los token JWT

--- src/config/env.js
+++ src/config/env.js
@@ -9,7 +9,7 @@ const config = {
   port: parseInt(process.env.PORT, 10) || 3000,
   nodeEnv: process.env.NODE_ENV || 'development',
   jwtSecret: process.env.JWT_SECRET || 'fallback_development_jwt_secret_123456_987654321',
-  jwtExpiresIn: process.env.JWT_EXPIRES_IN || '24h',
+  jwtExpiresIn: process.env.JWT_EXPIRES_IN || '12h',
   db: {
     host: process.env.DB_HOST || '',
     port: parseInt(process.env.DB_PORT, 10) || 3306,
